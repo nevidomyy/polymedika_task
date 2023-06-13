b58bc96a995318b7aefedca63213223bc528522a
nevidomyy <nevidom4ik@list.ru>
2023-06-13 21:14:55 +0300

    fix comment line

--- task_answers/SQL Queryes for task.sql
+++ task_answers/SQL Queryes for task.sql
@@ -1,4 +1,4 @@
-1.Выбрать всех студентов, обучающихся на курсе "Математика":
+--1.Выбрать всех студентов, обучающихся на курсе "Математика":
 SELECT
 	s.first_name as first_name,
 	s.last_name as last_name,
@@ -7,7 +7,7 @@ SELECT
 	FROM student s
 	LEFT JOIN course c ON s.course_id = c.id WHERE c.course = 'Математика'
 
-2.Обновить оценку студента по курсу:
+--2.Обновить оценку студента по курсу:
 Обновить оценку у конкретного студента по курсу
 UPDATE
 	grades g
@@ -20,7 +20,7 @@ UPDATE
 	WHERE s.id = 1 AND c.course = 'Программирование'
 	)
 
-Получить оценки всех студентов по курсу
+--Получить оценки всех студентов по курсу
 SELECT
 	s.id as id,
 	s.first_name as firs_name,
@@ -33,7 +33,7 @@ SELECT
 	LEFT JOIN academic_plan ap ON e.id = ap.exam_id
 	LEFT JOIN course c ON ap.course_id = c.id
 
-3.Выбрать всех преподавателей, которые преподают в здании №1.
+--3.Выбрать всех преподавателей, которые преподают в здании №1.
 SELECT
     p.id as id,
     p.first_name as first_name,
@@ -44,8 +44,8 @@ LEFT JOIN build b ON b.id = sch.build_id
 LEFT JOIN professor p ON p.id = sch.professor_id
 WHERE b.number = 1
 
-4.Удалить задание для самостоятельной работы, которое было создано более года назад.
+--4.Удалить задание для самостоятельной работы, которое было создано более года назад.
 DELETE FROM self_work WHERE created < NOW() - INTERVAL '1 year';
 
-5.Добавить новый семестр в учебный год.
+--5.Добавить новый семестр в учебный год.
 INSERT INTO semester (title) VALUES ('Третий семестр')
